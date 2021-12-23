1e018080a5bd793a22e86ae90ffebde76411d87d
lextri980 <lextri980@gmail.com>
2021-12-23 12:27:42 +0700

    authentication

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { MessengerPopupContext } from './components/Navbar/PopupMessenger/store/MessengerPopupContext';
+import { MessengerPopupContext } from "./components/Navbar/PopupMessenger/store/MessengerPopupContext";
 import App from "./App";
 import AuthContextProvider from "./pages/Login/AuthContext";
 
@@ -10,13 +10,13 @@ ReactDOM.render(
     <AuthContextProvider>
       <BrowserRouter>
         <MessengerPopupContext>
-      <App />
-    </MessengerPopupContext>
+          <App />
+        </MessengerPopupContext>
       </BrowserRouter>
     </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
-
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
